cb65b904198037b7d3d1a0bc8e2789c3c454efb7
Alejandro Ochoa <alejandro8ap@gmail.com>
2026-05-30 15:08:32 -0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,5 +12,22 @@ export default defineConfig({
   server: {
     port: 5173,
     host: true,
-  }
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        secure: false,
+      },
+      '/media': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        secure: false,
+      },
+      '/admin': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
